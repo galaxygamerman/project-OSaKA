df2cb40d1012e52ab53acfde139fe85d1c9e8d3b
SuzaanAhmed <SuzaanAhmed00@gmail.com>
2024-10-29 10:59:53 +0530

    Why was there an extra file called new.js? It says I am the author. I do not recollect any of it. Deleting it makes mo difference to the code.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
