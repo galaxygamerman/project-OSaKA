5773630c471d10207ab2ed20f1a0f5def1829c50
SuzaanAhmed <SuzaanAhmed00@gmail.com>
2024-10-29 11:11:44 +0530

    Basic Body shit, will css it later

--- webapp/src/UsePages/Waiter.js
+++ webapp/src/UsePages/Waiter.js
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 const Waiter = () => {
   return (
     <div>
     <h2>Waiter Page</h2>
+      <Card style={{width: '18rem'}}>
+        <Card.Body>
+          <Card.Title>
+            Item Name
+          </Card.Title>
+          <Card.Text>
+            TCost <br/>
+            <Button variant="primary" size="sm">
+              -
+            </Button>
+            Qty 
+            <Button variant="secondary" size="sm">
+              +
+            </Button>
+            ($Relative Cost)
+          </Card.Text>
+        </Card.Body>    
+      </Card>
     
-  </div>
+    </div>
   )
 }
 
