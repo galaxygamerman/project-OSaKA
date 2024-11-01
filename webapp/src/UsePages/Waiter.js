ad3411dfa3c0251dfc481b2cace09f1d97f92b8b
SuzaanAhmed <SuzaanAhmed00@gmail.com>
2024-11-01 11:26:27 +0530

    Done with the cards and applying useState to it. Now on to final send and storing it in DBS.

--- webapp/src/UsePages/Waiter.js
+++ webapp/src/UsePages/Waiter.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './Waiter.css';
+
+
 const Waiter = () => {
+
+  const [Qty,setQty] =useState(0)
+  const [price,setPrice]=useState(0)
+  const MainPrice=120
+
+/*Initially tried:
+    setQty(prevQty => prevQty+1)
+    setPrice(prevPrice =>(Qty*MainPrice))
+Did not work as it would alter quantity but the respective price would be diplayed after the queantity event and 
+present price displayed with respect to the previous quantity*/
+  function increment() {
+    setQty((prevQty) => {
+      const newQty = prevQty + 1;
+      setPrice(newQty * MainPrice);
+      return newQty;
+    });
+  }
+
+  function decrement() {
+    setQty((prevQty) => {
+      if (prevQty) {
+        const newQty = prevQty - 1;
+        setPrice(newQty * MainPrice);
+        return newQty;
+      }
+      return prevQty;
+    });
+  }
+
   return (
     <div>
     <h2>Waiter Page</h2>
@@ -11,15 +43,15 @@ const Waiter = () => {
             Item Name
           </Card.Title>
           <Card.Text>
-            TCost <br/>
-            <Button variant="primary" size="sm">
+            <div className="m-cost">₹{MainPrice}</div>
+            <Button onClick={decrement} variant="primary" size="sm">
               -
             </Button>
-            Qty 
-            <Button variant="secondary" size="sm">
+            <span>{Qty}</span> 
+            <Button onClick={increment} variant="secondary" size="sm">
               +
             </Button>
-            ($Relative Cost)
+            <span>₹{price}</span>
           </Card.Text>
         </Card.Body>    
       </Card>
