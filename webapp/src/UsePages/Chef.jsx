7a461aceebc08abaf43503ad86f577fa528c4ec5
galaxygamerman <mohdimadiqbal@gmail.com>
2024-11-07 22:18:49 +0530

    Fixed the bug with cards being too thin if number of cards was low, and removed unused css classes

--- webapp/src/UsePages/Chef.jsx
+++ webapp/src/UsePages/Chef.jsx
@@ -13,9 +13,9 @@ const Chef = () => {
   async function getJobs() {
     try {
       const response = await axios.get(`${process.env.REACT_APP_BACKEND_URI}/items`);
-      let tempQ = [...response.data].filter(data => data.status !== 'Delivered')
+      let tempQ = [...response.data].filter(data => data.status === 'Pending')
       setQueue(tempQ);
-      console.log(response.data);
+      // console.log(response.data);
       return tempQ;
     } catch (error) {
       console.error("Error fetching jobs:", error);
@@ -60,39 +60,43 @@ const Chef = () => {
       </nav>
       {/* Main content section */}
       <div className="container py-5">
-        <h2 className="text-center mb-5 text-primary"style={{ color: '#007bff' }}>Chef's Order Queue</h2>
-        <div className="row g-3">
-          {queue.map((order, index) => (
-            <div key={order.name} className="col-sm-12 col-md-6 col-lg-4 col-xxl-3">
-              <div className="card h-100 shadow-sm shadow-lg">
-                <div className="card-body h-100">
-                  <h3 className="card-title text-center mb-3 text-dark fw-bold">
-                    {order.name}
-                  </h3>
-                  <div className="card-text">
-                    <p className="fw-bold mb-2">Items:</p>
-                    <ul className="list-group list-group-numbered mb-3">
-                      {order.items.map((item, itemIndex) => (
-                        <li key={itemIndex} className="d-flex list-group-item bg-light justify-content-between">
-                          <div className="justify-content-left">{item.name}</div>
-                          <div className="justify-content-end">{item.quantity}</div>
-                        </li>
-                      ))}
-                    </ul>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <span className="badge bg-primary rounded-pill fs-6">
-                        ${order.totalPrice}
-                      </span>
-                      <button className="btn btn-success btn-sm" onClick={() => changeStatusToCooked(index)}>
-                        Mark as Complete
-                      </button>
+        <h2 className="text-center mb-5 text-primary">Chef's Order Queue</h2>
+        {queue.length > 0 ? (
+          <div className="row g-3 justify-content-center">
+            {queue.map((order, index) => (
+              <div key={order.name} className="col-sm-12 col-md-auto">
+                <div className="card h-100 shadow-sm shadow-lg">
+                  <div className="card-body h-100">
+                    <h3 className="card-title text-center mb-3 text-dark fw-bold">
+                      {order.name}
+                    </h3>
+                    <div className="card-text">
+                      <p className="fw-bold mb-2">Items:</p>
+                      <ul className="list-group list-group-numbered mb-3">
+                        {order.items.map((item, itemIndex) => (
+                          <li key={itemIndex} className="d-flex list-group-item bg-light justify-content-between">
+                            <div className="justify-content-left">{item.name}</div>
+                            <div className="justify-content-end">{item.quantity}</div>
+                          </li>
+                        ))}
+                      </ul>
+                      <div className="d-flex justify-content-between align-items-center">
+                        <span className="badge bg-primary rounded-pill fs-6">
+                          ${order.totalPrice}
+                        </span>
+                        <button className="btn btn-success btn-sm" onClick={() => changeStatusToCooked(index)}>
+                          Mark as Complete
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-center">The cooking queue is empty.</p>
+        )}
       </div>
     </>
   );
